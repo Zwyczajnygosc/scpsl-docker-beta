8c459e9a9490b196e76e84094fef1d54b86b6e14
Zwyczajnygosc <106614449+Zwyczajnygosc@users.noreply.github.com>
2024-12-01 09:24:00 +0100

    Update install-server.sh

--- build/scripts/install-server.sh
+++ build/scripts/install-server.sh
@@ -5,7 +5,7 @@
   "${STEAM_DIR}"/steamcmd.sh \
     +force_install_dir "${WORK_DIR}/${SERVER_DIR}" \
     +login "anonymous" \
-    +app_update "996560" validate \
+    +app_update "996560" -beta early-server-build \
     +quit
 
 )
